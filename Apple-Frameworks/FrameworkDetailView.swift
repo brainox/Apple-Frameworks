e87dee037f9e4cd40cdea524720db3dc579cff1e
Obinna <obinna.aguwa@gmail.com>
2022-11-10 13:24:46 +0100

    Showing the apple docs of selected framework using SafariViewController

--- Apple-Frameworks/FrameworkDetailView.swift
+++ Apple-Frameworks/FrameworkDetailView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct FrameworkDetailView: View {
     var framework: Framework
     @Binding var isShowingDetailView: Bool
+    @State private var isShowingSafariView = false
     
     var body: some View {
         VStack {
@@ -36,11 +37,13 @@ struct FrameworkDetailView: View {
             Spacer()
             
             Button {
-                
+                isShowingSafariView = true
             } label: {
                 AFButton(title: "Learn More")
             }
-
+        }
+        .fullScreenCover(isPresented: $isShowingSafariView) {
+            SafariView(url: URL(string: framework.urlString) ?? URL(string: "www.apple.com")!)
         }
     }
 }
---
+++ Apple-Frameworks/SafariView.swift
@@ -0,0 +1,22 @@
+//
+//  SafariView.swift
+//  Apple-Frameworks
+//
+//  Created by Obinna Aguwa on 10/11/2022.
+//
+
+import SwiftUI
+import SafariServices
+
+struct SafariView: UIViewControllerRepresentable {
+    
+    let url: URL
+    
+    func makeUIViewController(context: UIViewControllerRepresentableContext<SafariView>) -> SFSafariViewController {
+        SFSafariViewController(url: url)
+    }
+    
+    func updateUIViewController(_ uiViewController: SFSafariViewController, context: UIViewControllerRepresentableContext<SafariView>) {
+        
+    }
+}
